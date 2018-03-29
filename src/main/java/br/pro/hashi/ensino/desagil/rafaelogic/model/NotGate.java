92c3b4d153adce047d63b87b67f758265c246faa
IagoMendes <iagorainhamendes@hotmail.com>
2018-03-29 08:31:24 -0300

    Eu e Vitor Satyro corrigimos durante atendimento

--- src/main/java/br/pro/hashi/ensino/desagil/rafaelogic/model/NotGate.java
+++ src/main/java/br/pro/hashi/ensino/desagil/rafaelogic/model/NotGate.java
@@ -1,26 +1,23 @@
 package br.pro.hashi.ensino.desagil.rafaelogic.model;
 
 public class NotGate extends Gate {
-	private Emitter[] emitters;
 	private NandGate nandgate;
 	
 	
 	public NotGate() {
-		emitters = new Emitter[2];
 		//Criando objeto da classe NandGate
 		nandgate = new NandGate();
 	}
 
 	@Override
 	public void connect(int pinIndex, Emitter emitter) {
-		emitters[pinIndex] = emitter;
+		//Conectando as duas saidas do nandgate em uma porta Nand no emmitters
+		nandgate.connect(0, emitter);
+		nandgate.connect(1, emitter);
 	}
 
 	@Override
 	public boolean read() {
-		//Conectando as duas saidas do nandgate em uma porta Nand no emmitters
-		nandgate.connect(0, emitters[0]);
-		nandgate.connect(1, emitters[0]);
 		//Retornando o objeto nand ja setado como Not
 		return nandgate.read();
 	}
