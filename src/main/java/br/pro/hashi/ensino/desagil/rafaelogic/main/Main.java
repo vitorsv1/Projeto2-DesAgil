f8038595a2e665e7fdd232ae48c6094aa64df64f
IagoMendes <iagorainhamendes@hotmail.com>
2018-04-08 17:06:15 -0300

    Inicio da Entrega 2

--- src/main/java/br/pro/hashi/ensino/desagil/rafaelogic/main/Main.java
+++ src/main/java/br/pro/hashi/ensino/desagil/rafaelogic/main/Main.java
@@ -1,7 +1,44 @@
 package br.pro.hashi.ensino.desagil.rafaelogic.main;
 
+import java.util.LinkedList;
+
+import javax.swing.JFrame;
+
+import br.pro.hashi.ensino.desagil.rafaelogic.model.AndGate;
+import br.pro.hashi.ensino.desagil.rafaelogic.model.NandGate;
+import br.pro.hashi.ensino.desagil.rafaelogic.model.NotGate;
+import br.pro.hashi.ensino.desagil.rafaelogic.model.OrGate;
+import br.pro.hashi.ensino.desagil.rafaelogic.model.XorGate;
+import br.pro.hashi.ensino.desagil.rafaelogic.model.Gate;
+import br.pro.hashi.ensino.desagil.rafaelogic.view.View;
+
+
 public class Main {
 	public static void main(String[] args) {
 		
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+
+				// Constroi Modelo
+				LinkedList<Gate> model = new LinkedList<>();
+				model.add(new AndGate());
+				model.add(new NandGate());
+				model.add(new NotGate());
+				model.add(new OrGate());
+				model.add(new XorGate());
+
+				// Constroi View
+				View view = new View(model);
+
+				// Configurando a janela
+				JFrame frame = new JFrame();
+            	frame.setContentPane(view);
+            	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            	frame.setResizable(false);
+            	frame.pack();
+            	frame.setVisible(true);
+			}
+		});
 	}
 }
---
+++ src/main/java/br/pro/hashi/ensino/desagil/rafaelogic/view/View.java
@@ -0,0 +1,57 @@
+package br.pro.hashi.ensino.desagil.rafaelogic.view;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.LinkedList;
+
+import javax.swing.BoxLayout;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+import br.pro.hashi.ensino.desagil.rafaelogic.model.Gate;
+
+public class View extends JPanel implements ActionListener {
+	private static final long serialVersionUID = 1L;
+	
+	private JComboBox<Gate> menu;
+	private LogicGateView logicGateView;
+
+	public View(LinkedList<Gate> model) {
+
+		menu = new JComboBox<>();
+
+		for(Gate gate: model) {
+			menu.addItem(gate);
+		}
+
+		//Layout da janela
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+		add(menu);
+
+		addLogicGateView(0);
+
+		// Usa Action Listner para a janela reagir a mudanças no menu
+		menu.addActionListener(this);
+	}
+
+	// Adiciona o subpainel de um gate a este painel.
+	private void addGateView(int index) {
+		Gate gate = menu.getItemAt(index);
+		logicPortView = new LogicPortView(gate);
+		add(logicPortView);
+	}
+
+	// Troca a interface do gate ja presente
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		remove(logicGateView);
+		int index = menu.getSelectedIndex();
+		addGateView(index);
+
+		((JFrame) SwingUtilities.getRoot(this)).pack();
+	}
+}
+
